3e5c8e3f70fb3faf438d785b91c045568f62f1fd
indunil-jay <indunil.jayz@gmail.com>
2024-08-14 11:49:32 +0530

    add desktop-nav

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+export const navigation = [
+  { id: 1, url: "/", name: "home", bgfill: false },
+  { id: 2, url: "/portfolio", name: "portfolio", bgfill: false },
+  { id: 3, url: "/our-process", name: "our process", bgfill: false },
+  { id: 4, url: "/about", name: "about", bgfill: false },
+  { id: 5, url: "/contact", name: "contact", bgfill: true },
+] as const;
