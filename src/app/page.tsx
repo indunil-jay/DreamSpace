62ac49e49e901bceac0d40258ca9b77b4d3788ec
indunil-jay <indunil.jayz@gmail.com>
2024-08-14 11:12:26 +0530

    add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import { IoSparklesSharp } from "react-icons/io5";
+
 export default function Home() {
-  return <main className="flex min-h-screen flex-col "></main>;
+  return (
+    <header className="pt-5 pb-3 h-[5rem] px-2 sm:px-6 relative">
+      <div className="flex items-center gap-2">
+        <span className="text-[1.875rem] font-semibold">DreamSpace</span>
+        <IoSparklesSharp className="size-4" />
+      </div>
+
+      <nav></nav>
+    </header>
+  );
 }
