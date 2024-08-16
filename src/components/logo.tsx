31622e42098f646fdd18b936780d0a8e386ed335
indunil-jay <indunil.jayz@gmail.com>
2024-08-16 21:40:37 +0530

    add logo as link to home

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,18 +1,14 @@
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 import { IoSparklesSharp } from "react-icons/io5";
 
-export const Logo = ({ color = "dark" }: { color?: "light" | "dark" }) => {
+export const Logo = () => {
   return (
-    <div
-      className={cn(
-        "flex items-center gap-2 z-[9999]",
-        color === "dark" ? "text-dark" : "text-white"
-      )}
-    >
+    <Link href="/" className={cn("flex items-center gap-2 z-[9999]")}>
       <span className="text-[1.575rem] md:text-[1.875rem] font-semibold ">
         DreamSpace
       </span>
       <IoSparklesSharp className="size-4" />
-    </div>
+    </Link>
   );
 };
